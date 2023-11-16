c163b82ef8ea17f4ec423cc3a5b398d17f70eb44
Aksha3001 <144931999+Aksha3001@users.noreply.github.com>
2023-11-16 13:30:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const scoreNumberAI = document.querySelector(".score__number__AI");
 const nextrulesbtn=document.querySelector(".next-btn");
 const userHurray = document.querySelector(".winning");
 const scoreboard = document.querySelector(".header");
-
+let ai_score = 0;
 
 
 // Game Logic
